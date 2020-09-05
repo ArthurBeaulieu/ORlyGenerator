8fe5ebd7a9d8b78072d7b7c629c255d1cf4d0541
Arthur Beaulieu <contact@abeaulieu.fr>
2020-09-05 14:28:39 +0200

    style improvement

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
+/*  -----  Text manipulation  -----  */
+
+// Method to init field in output and set listener on input
 const editTextListener = (input, element) => {
   // Init element with its input default value
   element.innerHTML = input.value;
-  // Add listener to update on typing
+  // Add listener to update output when typing
   input.addEventListener('input', () => {
     element.innerHTML = input.value;
   });
 };
-
-/*  -----  Text manipulation  -----  */
-
+// Header, italic on top of output
 const header = document.getElementById('preview-header');
 const headerInput = document.getElementById('input-header');
-
+editTextListener(headerInput, header);
+// Title, in colored jumbotron
 const title = document.getElementById('preview-title');
 const titleInput = document.getElementById('input-title');
-
+editTextListener(titleInput, title);
+// Subtitle, right aligned under the jumbotron
 const subtitle = document.getElementById('preview-subtitle');
 const subtitleInput = document.getElementById('input-subtitle');
-
-editTextListener(headerInput, header);
-editTextListener(titleInput, title);
 editTextListener(subtitleInput, subtitle);
 
 /*  -----  Color manipulation  -----  */
 
+// Output colored elements
 const topBorder = document.getElementById('preview-top-border');
 const titleContainer = document.getElementById('preview-title-container');
 const questionMark = document.getElementById('orly-sup-text');
-
+// Add listener on color input to update output colors
 const colorPicker = document.getElementById('input-color');
 colorPicker.addEventListener('input', () => {
   topBorder.style.backgroundColor = colorPicker.value;
@@ -37,33 +38,48 @@ colorPicker.addEventListener('input', () => {
 
 /*  -----  Image manipulation  -----  */
 
+//Image related elements
 const image = document.getElementById('main-image');
 const imageSelect = document.getElementById('select-image');
 const imageFlip = document.getElementById('flip-image');
-
+const imageVerticalFlip = document.getElementById('v-flip-image');
+// Change animal on output
 imageSelect.addEventListener('input', () => {
   image.src = `img/animals/${imageSelect.value}.jpg`;
 });
-
+// Flip image horizontally
+let flipped = false;
 imageFlip.addEventListener('click', () => {
-  image.style.transform = `scaleX(${imageFlip.checked ? -1 : 1})`;
+  flipped = !flipped
+  image.style.transform = `scaleX(${flipped ? -1 : 1}) scaleY(${vFlipped ? -1 : 1})`;
+});
+// Flip image vertically
+let vFlipped = false;
+imageVerticalFlip.addEventListener('click', () => {
+  vFlipped = !vFlipped;
+  image.style.transform = `scaleX(${flipped ? -1 : 1}) scaleY(${vFlipped ? -1 : 1})`;
 });
 
 /*  -----  Image downloading  -----  */
 
+// Convert output DOM element into an image that is downloaded from current browser
 const download = document.getElementById('download');
 download.addEventListener('click', () => {
-  image.crossOrigin = 'Anonymous'; /* Set anonymous on image to allow downloading on local web server */
+  // Set anonymous on image to allow downloading on local web server
+  image.crossOrigin = 'Anonymous';
+  // Execute html2canvas with output div
   html2canvas(document.getElementById('output'), {
-    logging: true,
-    allowTaint: true,
-    useCORS: true,
-    scale: 4
+    logging: false, // Make html2canvas silent on execution
+    scale: 4, // Ensure a good output resolution
+    useCORS: true, // Allow CORS for animal image
+    allowTaint: true // Allow tainted canvas because of loading an image into it
   }).then(canvas => {
+    // Create virtual downloading link
     const link = document.createElement('A');
     link.download = 'orly-generated.png';
     link.href = canvas.toDataURL('image/png').replace("image/jpeg", "image/octet-stream");
     link.click();
-    setTimeout(() => { image.crossOrigin = null }, 100); /* Reset CORS policy to allow image swapping in UI */
+    // Reset CORS policy to allow image swapping in UI
+    setTimeout(() => { image.crossOrigin = null }, 100);
   });
 });

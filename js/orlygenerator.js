05a9543491f478b17df21dfdae2b887e408badf5
ArthurBeaulieu <contact@abeaulieu.fr>
2023-03-12 14:32:24 +0100

    fix: mobile detection

--- js/orlygenerator.js
+++ js/orlygenerator.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const mobileCheck = () => {
-  return navigator.userAgentData.mobile;
+  let hasTouchScreen = false;
+  if ('maxTouchPoints' in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if ('msMaxTouchPoints' in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else {
+    const mQ = matchMedia?.('(pointer:coarse)');
+    if (mQ?.media === '(pointer:coarse)') {
+      hasTouchScreen = !!mQ.matches;
+    } else if ('orientation' in window) {
+      hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      const UA = navigator.userAgent;
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+  }
+
+  return hasTouchScreen;
 };
 
 /*  -----  Text manipulation  -----  */
